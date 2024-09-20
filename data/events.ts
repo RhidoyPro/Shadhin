de4d082133d6104ca78aff0b2b76c367a8ef146d
Sheharyar Ishfaq <sheharyarishfaq@gmail.com>
2024-09-20 17:16:17 +0500

    Showing only the all-states status in the status page

--- data/events.ts
+++ data/events.ts
@@ -1,38 +1,8 @@
 import { db } from "@/lib/db";
 import { EventStatus } from "@prisma/client";
-import BangladeshStates from "./bangladesh-states";
 
 export const getEventsByState = async (stateName: string) => {
   try {
-    if (stateName === BangladeshStates[0].slug) {
-      const events = await db.event.findMany({
-        orderBy: {
-          createdAt: "desc",
-        },
-        include: {
-          user: true,
-          likes: {
-            select: {
-              id: true,
-            },
-          },
-          attendees: {
-            where: {
-              status: EventStatus.GOING,
-            },
-            select: {
-              id: true,
-            },
-          },
-          comments: {
-            select: {
-              id: true,
-            },
-          },
-        },
-      });
-      return events;
-    }
     const events = await db.event.findMany({
       where: {
         stateName,
