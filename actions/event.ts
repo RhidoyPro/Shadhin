e90af7eddc5c1ef8749d629a47ac5e142cc0730b
Sheharyar Ishfaq <sheharyarishfaq@gmail.com>
2024-09-27 20:20:12 +0500

    Removed the update state to district code

--- actions/event.ts
+++ actions/event.ts
@@ -251,14 +251,6 @@ export const fetchEvents = async (
   page?: number,
   limit?: number
 ) => {
-  await db.event.updateMany({
-    where: {
-      stateName: "all-states",
-    },
-    data: {
-      stateName: "all-districts",
-    },
-  });
   const session = await auth();
   const events =
     (await getEventsByStatePaginated(stateName, page, limit)) || [];
